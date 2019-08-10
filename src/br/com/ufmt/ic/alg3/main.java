2027d050b7a4e3ae17c88daaf657c231d1db58d0
João <joogabrielsntn66@gmail.com>
2019-08-10 12:13:53 -0400

    Tela principal

---
+++ src/br/com/ufmt/ic/alg3/main.java
@@ -0,0 +1,10 @@
+package br.com.ufmt.ic.alg3;
+
+import br.com.ufmt.ic.alg3.view.Home;
+
+public class main {
+    public static void main(String[] args) {
+        Home tela = new Home();
+        tela.setVisible(true);
+    }
+}

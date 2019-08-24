8caf4a708dfb5ddde94c103232456842a757c165
Leiciane Pereira <leicyani2011@gmail.com>
2019-08-24 13:25:05 -0400

    Implementado Cadastrar Profissional

--- src/alg/jkl/system/models/Profissional.java
+++ src/alg/jkl/system/models/Profissional.java
@@ -1,4 +1,32 @@
 package alg.jkl.system.models;
 
 public class Profissional { 
+    private String cpf;
+    private String nome;
+    private String funcao;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+    
 }

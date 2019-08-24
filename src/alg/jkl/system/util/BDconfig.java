a1d47c524a56b19bea9a4a983a4787222d525aaa
João <joogabrielsntn66@gmail.com>
2019-08-24 08:53:11 -0400

    implementado arquivo de configuração para conexao banco de dados

--- src/alg/jkl/system/util/BDconfig.java
+++ src/alg/jkl/system/util/BDconfig.java
@@ -1,21 +1,44 @@
 package alg.jkl.system.util;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class BDconfig {
-    private static final String _BDhost = "localhost";
-    private static final String _BDname = "jkl";
-    private static final String _BDuser = "root";
-    private static final String _BDpass = "jump55";
+    private static String BDhost;
+    private static String BDname;
+    private static String BDuser;
+    private static String BDpass;
+    
+    private static Properties carregarProp() {
+        Properties props = new Properties();
+        try {
+            FileInputStream file = new FileInputStream("./nbproject/bd.properties");
+            props.load(file);
+            return props;
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
     
     public static Connection conectar() {
         Connection conexao = null;
-        String urlCon = "jdbc:mysql://" + _BDhost + ":3306/" + _BDname;
+        Properties props = carregarProp();
+        BDhost = props.getProperty("db.host");
+        BDname = props.getProperty("db.name");
+        BDuser = props.getProperty("db.user");
+        BDpass = props.getProperty("db.pass");
+        String urlCon = "jdbc:mysql://" + BDhost + ":3306/" + BDname;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            conexao = DriverManager.getConnection(urlCon, _BDuser, _BDpass); 
+            conexao = DriverManager.getConnection(urlCon, BDuser, BDpass); 
         } 
         catch(SQLException e) {
             System.out.println("Erro conectando ao banco: " + e.getMessage());
@@ -24,5 +47,5 @@ public class BDconfig {
             System.out.println("Classe não encontrada: " + e.getMessage());            
         }
         return conexao;
-    }
+    } 
 }
